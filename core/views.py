30bb0ec435a27ad6ff6ade5d10e75147832b6b9c
shehjad0 <bettercallshehjad@gmail.com>
2024-01-24 18:46:11 +0600

    UI updates

--- core/views.py
+++ core/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def about(request):
+    return render(request, 'about.html')
--- quiz_zone/urls.py
+++ quiz_zone/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from core.views import index
+from core.views import index, faq, about
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', index, name='home'),
+    path('faq/', faq, name='faq'),
+    path('about/', about, name='about'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('quiz/', include('quiz.urls')),
